48b0b2a245e9f7a101b590a5528c0e8ab9bc2798
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2019-02-02 21:40:49 +0300

    add typecpecs for vertex module

--- lib/gruf/model/vertex.ex
+++ lib/gruf/model/vertex.ex
@@ -3,8 +3,19 @@ defmodule Gruf.Model.Vertex do
 
   alias Gruf.Model.Edge
 
+  @type type() :: :initial
+                | :regular
+
+  @type t() :: %Gruf.Model.Vertex{
+    id: String.t(),
+    type: Vertex.type(),
+    data: map(),
+    edges: map()
+  }
+
   defstruct [:id, :type, :data, :edges]
 
+  @spec new(Vertex.type(), map()) :: Vertex.t()
   def new(type, initial_data) do
     vertex_id = gen_id()
     edge = Edge.new(:phantom, vertex_id)
@@ -18,6 +29,7 @@ defmodule Gruf.Model.Vertex do
   end
 
   # TODO: handle error
+  @spec update_phantom_edge(Vertex.t(), String.t()) :: Vertex.t()
   def update_phantom_edge(vertex, to_vertex_id) do
     new_phantom_edge = get_phantom_edge(vertex)
       |> Edge.update_phantom_edge(to_vertex_id)
@@ -25,6 +37,7 @@ defmodule Gruf.Model.Vertex do
     update_edge(vertex, new_phantom_edge)
   end
 
+  @spec get_phantom_edge(Vertex.t()) :: Edge.t()
   def get_phantom_edge(vertex) do
     vertex.edges
       |> Map.values()
@@ -32,11 +45,13 @@ defmodule Gruf.Model.Vertex do
   end
 
   # TODO: handle error
+  @spec update_edge(Vertex.t(), Edge.t()) :: Vertex.t()
   def update_edge(vertex, edge) do
     new_edges = Map.put(vertex.edges, edge.id, edge)
     %{vertex | edges: new_edges}
   end
 
+  @spec gen_id() :: String.t()
   def gen_id() do
     ulid = Ulid.generate()
     "vertex:#{ulid}"
